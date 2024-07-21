bee095ecb5080dd6daa9b9da306a44d51bcffb3c
jailcomfranssa <jail_son_0@hotmail.com>
2024-07-21 18:17:20 -0300

    correções page resume TabsContent education

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -6,6 +6,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
+import Image from 'next/image';
 
 /** About dados */
 const about = {
@@ -69,6 +70,7 @@ const education = {
             institution: "Instituto Federal da Paraíba (IFPB)",
             degree: "Sistema Para Internet",
             duration: "2018 - 2023",
+            image: "/images/logo-ifpb.png" 
         },
     ]
 }
@@ -150,22 +152,34 @@ const Resume = () => {
                             </div>
                         </TabsContent>
 
-                        {/**EDUCAÇÂO */}
-                        <TabsContent value="education" className="w-full">
-                        <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                        
+                       {/** EDUCAÇÃO */}
+                    <TabsContent value="education" className="w-full">
+                            <div className='flex flex-col gap-[30px] text-center xl:text-left'>
                                 <h3 className='text-4xl front-bold'>{education.title}</h3>
                                 <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0' dangerouslySetInnerHTML={{ __html: education.description }}></p>
                                 <ScrollArea className="h-[300px]">
                                     <ul className='grid grid-cols-1 lg:grid-cols-1 gap-[25px]'>
-                                        {education.items.map((item, index)=>{
-                                            return <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
-                                                <span className='text-accent'>{item.duration}</span>
-                                                <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left'>{item.degree}</h3>
-                                                <div className='flex items-center gap-3'>
-                                                    <span className='w-[6px] h-[6px] rounded-full bg-accent'></span>
-                                                    <p className='text-white/60'>{item.institution}</p>
-                                                </div>
-                                            </li>
+                                        {education.items.map((item, index) => {
+                                            return (
+                                                <li key={index} className='bg-[#232329] sm:h-[350px] lg:h-[200px] xl:h-[184px]  py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:justify-between items-center lg:items-start gap-1 lg:flex-row-reverse'>
+                                                        <div className='w-[200px] h-[130px] rounded-full mb-4 lg:mb-0 lg:ml-4 relative'>
+                                                            <Image
+                                                            src={item.image}  // URL da sua image
+                                                            alt={item.institution}
+                                                            layout="fill"
+                                                            objectFit="cover"
+                                                            className='rounded-full'
+                                                            />
+                                                        </div>
+                                                    <div className='flex flex-col items-center lg:items-start'>
+                                                        <span className='text-accent'>{item.duration}</span>
+                                                        <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left'>{item.degree}</h3>
+                                                        <p className='text-white/60'>{item.institution}</p>
+                                                    </div>
+                                                
+                                                </li>
+                                            );
                                         })}
                                     </ul>
                                 </ScrollArea>
