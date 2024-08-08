1b35776f69efd38bc0bae0a6fa18069669c3e0f8
jailcomfranssa <jail_son_0@hotmail.com>
2024-08-08 05:34:12 -0300

    correção page contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -17,13 +17,13 @@ import { motion } from "framer-motion";
 const info = [
     {
         icon:<FaPhoneAlt/>,
-        title: "Phone",
+        title: "Telefone",
         description: "(+55) 83 99942-3019"
     },
     {
         icon:<FaEnvelope/>,
         title: "Email",
-        description: "jailson@example.com"
+        description: "jailson.franca@dcx.ufpb.br"
     },
     {
         icon:<FaMapMarkedAlt/>,
@@ -45,8 +45,8 @@ const Contact = () => {
                     {/**Form */}
                     <div className="xl:w-[74%] order-2 xl:order-none">
                         <form className="flex flex-col gap-2 p-2 bg-[#27272c] rounded-xl">
-                            <h3 className="text-4xl text-accent">Let's work together</h3>
-                            <p className="text-nowrap/60">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                            <h3 className="text-4xl text-accent">Você tem alguma dúvida ?</h3>
+                            <p className="text-nowrap/60">MANDE-ME UM EMAIL</p>
                             {/**input */}
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 <Input type="nome" placeholder="Nome"/>
